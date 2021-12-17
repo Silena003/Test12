0cd7ebe5213ffb463e343fc414b05aafe3630e90
Elena <silena003@gmail.com>
2021-12-17 15:07:51 +0300

    Initial commit

---
+++ src/test/java/ru/netology/domain/RadioTest.java
@@ -0,0 +1,33 @@
+package ru.netology.domain;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class RadioTest {
+    @Test
+    public void constructorVolume() {
+        Radio radio = new Radio(100, 100, 0, true);
+
+        assertEquals(100, radio.getCurrentVolume());
+    }
+
+
+    @Test
+    public void constructorRadioStation() {
+        Radio radio = new Radio(8, 9, 0);
+
+        assertEquals(8, radio.getCurrentRadioStation());
+
+    }
+
+
+//    @Test
+//    public void Ra() {
+//        Radio radio = new Radio();
+//        assertEquals(10, radio.getCurrentVolume());
+//        assertFalse(radio.isOn());
+
+}
+
